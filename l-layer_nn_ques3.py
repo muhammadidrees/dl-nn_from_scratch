02b4cb4746c6e886ed4cbb5668cb5754a6a76d0c
Muhammad Idrees <midrees097@gmail.com>
2021-11-20 22:43:04 +0500

    minor fixes to make the code work

---
+++ l-layer_nn_ques3.py
@@ -0,0 +1,154 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+import itertools
+
+np.set_printoptions(threshold=np.inf)
+
+def Sigmoid(Z):
+    return 1/(1+np.exp(-Z))
+
+def Relu(Z):
+    return np.maximum(0,Z)
+
+def dRelu2(dZ, Z):    
+    dZ[Z <= 0] = 0    
+    return dZ
+
+def dRelu(x):
+    x[x<=0] = 0
+    x[x>0] = 1
+    return x
+
+def dSigmoid(Z):
+    s = 1/(1+np.exp(-Z))
+    dZ = s * (1-s)
+    return dZ
+
+class dlnet:
+    def __init__(self, x, y, L):
+        self.debug = 0;
+        self.X=x
+        self.Y=y
+        self.Yh=np.zeros((1,self.Y.shape[1])) 
+        self.L=L
+        self.dims = [9, 15, 1] 
+        self.param = {}
+        self.ch = {}
+        self.grad = {}
+        self.loss = []
+        self.lr=0.003
+        self.sam = self.Y.shape[1]
+        self.threshold=0.5
+        
+    def nInit(self):    
+        np.random.seed(1)
+        self.param['W1'] = np.random.randn(self.dims[1], self.dims[0]) / np.sqrt(self.dims[0]) 
+        self.param['b1'] = np.zeros((self.dims[1], 1))        
+        self.param['W2'] = np.random.randn(self.dims[2], self.dims[1]) / np.sqrt(self.dims[1]) 
+        self.param['b2'] = np.zeros((self.dims[2], 1))                
+        return 
+
+    def forward(self):    
+        Z1 = self.param['W1'].dot(self.X) + self.param['b1'] 
+        A1 = Relu(Z1)
+        self.ch['Z1'],self.ch['A1']=Z1,A1
+        
+        Z2 = self.param['W2'].dot(A1) + self.param['b2']  
+        A2 = Sigmoid(Z2)
+        self.ch['Z2'],self.ch['A2']=Z2,A2
+
+        self.Yh=A2
+        loss=self.nloss(A2)
+        return self.Yh, loss
+
+    def nloss(self,Yh):
+        loss = (1./self.sam) * (-np.dot(self.Y,np.log(Yh).T) - np.dot(1-self.Y, np.log(1-Yh).T))    
+        return loss
+
+    def backward(self):
+        dLoss_Yh = - (np.divide(self.Y, self.Yh ) - np.divide(1 - self.Y, 1 - self.Yh))    
+        
+        dLoss_Z2 = dLoss_Yh * dSigmoid(self.ch['Z2'])    
+        dLoss_A1 = np.dot(self.param["W2"].T,dLoss_Z2)
+        dLoss_W2 = 1./self.ch['A1'].shape[1] * np.dot(dLoss_Z2,self.ch['A1'].T)
+        dLoss_b2 = 1./self.ch['A1'].shape[1] * np.dot(dLoss_Z2, np.ones([dLoss_Z2.shape[1],1])) 
+                            
+        dLoss_Z1 = dLoss_A1 * dRelu(self.ch['Z1'])        
+        dLoss_A0 = np.dot(self.param["W1"].T,dLoss_Z1)
+        dLoss_W1 = 1./self.X.shape[1] * np.dot(dLoss_Z1,self.X.T)
+        dLoss_b1 = 1./self.X.shape[1] * np.dot(dLoss_Z1, np.ones([dLoss_Z1.shape[1],1]))  
+        
+        self.param["W1"] = self.param["W1"] - self.lr * dLoss_W1
+        self.param["b1"] = self.param["b1"] - self.lr * dLoss_b1
+        self.param["W2"] = self.param["W2"] - self.lr * dLoss_W2
+        self.param["b2"] = self.param["b2"] - self.lr * dLoss_b2
+        
+        return
+
+
+    def pred(self,x, y):  
+        self.X=x
+        self.Y=y
+        comp = np.zeros((1,x.shape[1]))
+        pred, loss= self.forward()    
+    
+        for i in range(0, pred.shape[1]):
+            if pred[0,i] > self.threshold: comp[0,i] = 1
+            else: comp[0,i] = 0
+    
+        print("Acc: " + str(np.sum((comp == y)/x.shape[1])))
+        
+        return comp
+    
+    def gd(self,X, Y, iter = 3000):
+        np.random.seed(1)                         
+    
+        self.nInit()
+    
+        for i in range(0, iter):
+            Yh, loss=self.forward()
+            self.backward()
+        
+            print ("Cost after iteration %i: %f" %(i, loss))
+            self.loss.append(loss)
+
+        plt.plot(np.squeeze(self.loss))
+        plt.ylabel('Loss')
+        plt.xlabel('Iter')
+        plt.title("Lr =" + str(self.lr))
+        plt.show()
+    
+        return 
+
+
+df = pd.read_csv('data.csv',header=None)
+df = df.astype(float)
+
+df.head(3)
+scaled_df=df
+names = df.columns[0:4]
+scaler = MinMaxScaler() 
+scaled_df = scaler.fit_transform(df.iloc[:,0:4]) 
+scaled_df = pd.DataFrame(scaled_df, columns=names)
+
+x=scaled_df.iloc[0:4,1:4].values.transpose()
+y=df.iloc[0:4,4:].values
+
+
+print(df.shape, x.shape, y.shape)
+
+nn = dlnet(x,y, 5)
+nn.lr=0.07
+nn.dims = [3, 2, 1] 
+
+nn.gd(x, y, iter = 10)
+
+nn.X,nn.Y=x, y 
+yvalh, loss = nn.forward()
+print("\ny",np.around(x[:,0:4,], decimals=0).astype(np.int))       
+print("\nyh",np.around(y[:,0:4,], decimals=0).astype(np.int),"\n")         
